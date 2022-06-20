0f02b6956e9b39a4ed50826c909aef3ca585eb9a
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2022-06-20 23:34:08 +0200

    Prepare for Swift Package Index
    - Add spi config
    - Update packages
    - Add some extra doc comments

--- Sources/StreamSDKVapor/StreamJWTPayload.swift
+++ Sources/StreamSDKVapor/StreamJWTPayload.swift
@@ -7,12 +7,12 @@ struct StreamPayload: JWTPayload {
         case userID = "user_id"
     }
 
-    // The "exp" (expiration time) claim identifies the expiration time on
-    // or after which the JWT MUST NOT be accepted for processing.
+    /// The "exp" (expiration time) claim identifies the expiration time on
+    /// or after which the JWT MUST NOT be accepted for processing.
     var expiration: ExpirationClaim?
 
-    // Custom data.
-    // If true, the user is an admin.
+    /// Custom data.
+    /// If true, the user is an admin.
     var userID: String
     
     func verify(using signer: JWTSigner) throws {
